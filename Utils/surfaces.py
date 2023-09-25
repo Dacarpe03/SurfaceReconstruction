5ace5cee57df53b171822ff18b4a7b63bd23c875
Dacarpe03 <d.carmonap97@gmail.com>
2023-09-25 11:42:21 +0200

    Delete surfaces from wrong directory and add check_zernike_parameters function

--- Utils/surfaces.py
+++ Utils/surfaces.py
@@ -1,3 +1,6 @@
+from constants import *
+
+
 def print_verbose_msg(msg, 
                       level):
     """
@@ -13,3 +16,37 @@ def print_verbose_msg(msg,
     
     complete_msg = f"{VERBOSITY_DICT[level]}: {msg}"
     print(complete_msg)
+    
+ 
+def check_zernike_parameters(m_index, 
+                             n_index, 
+                             rho, 
+                             verbose=False):
+    """
+    This function checks that the zernike parameters are valid
+    
+    Input:
+        m_index (int): The m number of the Zernike Polynomial
+        n_index (int): The n number of the Zernike Polynomial
+        rho (float): The distance of the point to the center (radius in polar coordinates) 
+        verbose (bool): Indicates if the errors are displayed in console. Default is False.
+    
+    Returns:
+        bool: True if the parameters are correct, False otherwise
+    """
+    
+    # If n is lower than m there is an error
+    if n_index < abs(m_index):
+        if verbose:
+            msg = f"The n index is smaller than m index: {n_index} < {abs(m_index)}"
+            print_verbose_msg(msg, ERROR)
+        return False
+    
+    # If the radius is less than 0 or greater than 1 there is an error
+    if rho < 0 or rho > 1:
+        if verbose:
+            msg = f"The radius (ro) must be between -1 and 1. Radius is {rho}"
+            print_verbose_msg(msg, ERROR)
+        return False
+    
+    return True
