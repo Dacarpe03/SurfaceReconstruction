3fb724054b9b2008488da981f50c18659a02f806
Dacarpe03 <d.carmonap97@gmail.com>
2023-09-25 13:45:21 +0200

    Add get_random_zernike_coefficient function

--- Utils/surfaces.py
+++ Utils/surfaces.py
@@ -150,6 +150,31 @@ def compute_radial_value(m_index,
     return r_value
 
 
+def get_random_zernike_coefficients():
+    """
+    Describes a random surface based on the zernike polynomials returning their indexes and coefficients to plot them
+    Input:
+        None
+    
+    Returns:
+        zernike_polynomials (list): A list of tuples with zernike polynomials info (m_index, n_index, coefficient)
+    """
+    
+    zernike_polynomials = []
+    
+    # Create the zernike tuples containing m index, n index and coefficient of the polynomial
+    for zpi in ZERNIKE_POLYNOMIALS_INDEXES:
+        # Get a random coefficient for the zernike polynomial
+        coef = random.uniform(COEFFICIENT_MIN, COEFFICIENT_MAX)
+        # Create the combined tuple
+        zp = zpi + (coef,)
+        # Append it to the list of zernike polynomials with coefficients
+        zernike_polynomials.append(zp)
+
+    return zernike_polynomials
+
+
+
 def polar_samples_unit_circle_for_data_generation(n_circles=5, 
                                                   n_samples=20):
     """
@@ -275,7 +300,7 @@ def compute_zernike_polynomial_for_meshgrid(m_index,
     return zernike_mesh
 
 
-    def plot_surface(zernike_polynomials,
+def plot_surface(zernike_polynomials,
                  n_radiuses=50,
                  n_angles=50,
                  verbose=False):
