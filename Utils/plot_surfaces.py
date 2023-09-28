d2b8296c7d781e1b162749fdd0580c7b02b78397
Dacarpe03 <d.carmonap97@gmail.com>
2023-09-28 08:24:37 +0200

    Add test function to plot with plotly

--- Utils/plot_surfaces.py
+++ Utils/plot_surfaces.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.graph_objecst as go
+import pandas as pd
 
 from constants import *
 from surfaces import *
@@ -126,3 +128,15 @@ def plot_2d_polar_points(
 
     # Plot the points
     ax.plot(varphi_samples, rho_samples, 'k.')
+
+
+def test_plot():
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+    z = z_data.values
+    sh_0, sh_1 = z.shape
+    x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+    fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                  width=500, height=500,
+                  margin=dict(l=65, r=50, b=65, t=90))
+    fig.show()
